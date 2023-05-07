60fc71f04a6acf18b5b5ffae9277f2a4f7f8b937
jfremond <jfremond@student.42.fr>
2023-05-08 00:07:15 +0200

    Doesn't work 502 bad gateway, PHP issue

--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -6,74 +6,44 @@
 #    By: jfremond <jfremond@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/05/04 09:41:13 by jfremond          #+#    #+#              #
-#    Updated: 2023/05/04 15:15:29 by jfremond         ###   ########.fr        #
+#    Updated: 2023/05/07 23:09:10 by jfremond         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 # #!/bin/bash
 
-# # To make sure MariaDB database is launched
-# sleep 10;
+# To make sure MariaDB database is launched
+sleep 10;
 
-# # Autoconfigure wordpress database if not already done
-# if [ ! -e /var/www/wordpress/wp-config.php ]; then
-# 	wp config create	--allow-root \
-# 						--dbname=$MYSQL_DATABASE \
-# 						--dbuser=$MYSQL_USER \
-# 						--dbpass=$MYSQL_PASSWORD \
-# 						--dbhost=mariadb:3306 --path='/var/www/wordpress'
-
-# # Autoconfigure website if not already done
-# sleep 2
-# wp core install	--url=$DOMAIN_NAME \
-# 				--title=$SITE_TITLE \
-# 				--admin_user=$ADMIN_USER \
-# 				--admin_password=$ADMIN_PASSWORD \
-# 				--admin_email=$ADMIN_EMAIL \
-# 				--allow-root \
-# 				--path='/var/www/wordpress'
-
-# # Create one USER
-# wp user create	--allow-root \
-# 				--role=author $USER1_LOGIN $USER1_EMAIL \
-# 				--user_pass=$USER1_PASSWORD \
-# 				--path='/var/www/wordpress' >> /log.txt
-# fi
-
-# # Create /run/php folder doesn't exist create it
-# if [ ! -d /run/php ]; then
-# 	mkdir ./run/php
-# fi
-
-# # Start php-fpm
-# /usr/sbin/php-fpm7.3 -F
-
-#!bin/bash
-sleep 10
+# Autoconfigure wordpress database if not already done
 if [ ! -e /var/www/wordpress/wp-config.php ]; then
-    wp config create	--allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD \
-    					--dbhost=mariadb:3306 --path='/var/www/wordpress'
+	wp config create	--allow-root \
+						--dbname=$MYSQL_DATABASE \
+						--dbuser=$MYSQL_USER \
+						--dbpass=$MYSQL_PASSWORD \
+						--dbhost=mariadb:3306 --path='/var/www/wordpress'
 
+# Autoconfigure website if not already done
 sleep 2
-wp core install     --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root --path='/var/www/wordpress'
-wp user create      --allow-root --role=author $USER1_LOGIN $USER1_MAIL --user_pass=$USER1_PASS --path='/var/www/wordpress' >> /log.txt
+wp core install	--url=$DOMAIN_NAME \
+				--title=$SITE_TITLE \
+				--admin_user=$ADMIN_USER \
+				--admin_password=$ADMIN_PASSWORD \
+				--admin_email=$ADMIN_EMAIL \
+				--allow-root \
+				--path='/var/www/wordpress'
+
+# Create one USER
+wp user create	--allow-root \
+				--role=author $USER1_LOGIN $USER1_EMAIL \
+				--user_pass=$USER1_PASSWORD \
+				--path='/var/www/wordpress' >> /log.txt
 fi
 
-# echo "define( 'CONCATENATE_SCRIPTS', false );" >> /var/www/wordpress/wp-config.php
-# echo "define( 'SCRIPT_DEBUG', true );" >> /var/www/wordpress/wp-config.php
-# echo "define( 'WP_HOME', 'https://jcluzet.42.fr' );" >> /var/www/wordpress/wp-config.php
-# echo "define( 'WP_SITEURL', 'https://jcluzet.42.fr' );" >> /var/www/wordpress/wp-config.php
-
-# echo "define( 'WP_DEBUG', true);" >> /var/www/wordpress/wp-config.php
-# echo "define( 'WP_DEBUG_LOG', true);" >> /var/www/wordpress/wp-config.php
-# echo "define( 'WP_DEBUG_DISPLAY', false);" >> /var/www/wordpress/wp-config.php
-# echo "define('WP_ALLOW_REPAIR', true);" >> /var/www/wordpress/wp-config.php
-
-	
-
-# if /run/php folder does not exist, create it
+# Create /run/php folder doesn't exist create it
 if [ ! -d /run/php ]; then
-    mkdir ./run/php
+	mkdir ./run/php
 fi
 
-/usr/sbin/php-fpm7.3 -F
+# Start php-fpm
+/usr/sbin/php-fpm7.3 -F -R
