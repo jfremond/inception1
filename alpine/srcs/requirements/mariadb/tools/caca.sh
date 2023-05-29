e438234e44d304605010bae7e48e5dd2c557462b
jfremond <jfremond@student.42.fr>
2023-05-29 21:28:46 +0200

    MariaDB left to do

--- alpine/srcs/requirements/mariadb/tools/caca.sh
+++ alpine/srcs/requirements/mariadb/tools/caca.sh
@@ -1,3 +1,10 @@
 #/!bin/sh
 
 while true; do echo "pouet"; done
+
+# Lancer mysql en arriere plan et attendre qu'il soit lance (demande a Google)
+# Installer bases de donnees (cherche la commande)
+# Config root (en SQL)
+# Config nouvel user (en SQL)
+# Change les droits de /var/lib/mysql
+# Kill proc mysql en arriere plan et le lancer en premier plan (recupere id processus avec "$!")
