69b9d621e259a91b214bfddc9e9991491311d1fb
jfremond <jfremond@student.42.fr>
2023-07-01 00:27:11 +0200

    :hammer: Update entrypoint.sh

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -12,37 +12,50 @@
 
 sleep 5
 
-#TODO (BIG TODO) write meaning of options used by mariadbd and mariadb-install-db
-
-#TODO remove these for testing
-touch tmp_file
-chmod 755 tmp_file
-
+# Set up database if not already setup
+# --user=mysql ->
+# --skip-networking=0 ->
 if [ -d /var/lib/mysql/mysql ]; then
 	echo "Already set up, starting..."
 	exec /usr/bin/mariadbd --user=mysql --skip-networking=0
 	exit 1
 fi
 
+# Install directory that'll contain mysql database and give ownership to user mysql
 echo "Installing..."
 mkdir -p /var/lib/mysql
 chown -R mysql:mysql /var/lib/mysql
 
+# --auth-root-authentication-method=normal -> 
+# --basedir=/usr ->
+# --datadir=/var/lib/mysql ->
+# --skip-test-db ->
+#--user=mysql ->
 mariadb-install-db --auth-root-authentication-method=normal --basedir=/usr --datadir=/var/lib/mysql --skip-test-db --user=mysql
 
 # Write setup database and users in temporary file
+#
+#
+#
+#
+#
 cat << break > tmp_file
 FLUSH PRIVILEGES;
-DELETE FROM mysql.user WHERE User = 'root' AND Host != 'localhost';
+DELETE FROM mysql.user WHERE User = 'root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$DB_ROOT_PASS');
 CREATE USER '$DB_USER_NAME'@'%' IDENTIFIED BY '$DB_USER_PASS';
 GRANT ALL ON $DB_NAME.* TO '$DB_USER_NAME'@'%' IDENTIFIED BY '$DB_USER_PASS';
-FLUSH PRIVILEGES;
 break
 
+# --user=mysql -> 
+# --bootstrap -> 
+# --skip-name-resolve -> 
+# --skip-networking=0 ->
 /usr/bin/mariadbd --user=mysql --bootstrap --skip-name-resolve --skip-networking=0 < tmp_file
 
 # Remove temporary file
 rm -f tmp_file
 
+# --user=mysql ->
+# --skip-networking=0 ->
 exec /usr/bin/mariadbd --user=mysql --skip-networking=0
