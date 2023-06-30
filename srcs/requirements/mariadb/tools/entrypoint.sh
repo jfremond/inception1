4a4e3f8ff19ae596af64c196f98391c9eb01bfa0
jfremond <jfremond@student.42.fr>
2023-06-30 20:59:37 +0200

    :memo: update entrypoint.sh

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,7 +1,22 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    entrypoint.sh                                      :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: jfremond <jfremond@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2023/06/30 20:51:33 by jfremond          #+#    #+#              #
+#    Updated: 2023/06/30 20:57:56 by jfremond         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
 sleep 5
 
-touch tmp_mdb
-chmod 755 tmp_mdb
+#TODO (BIG TODO) write meaning of options used by mariadbd and mariadb-install-db
+
+#TODO remove these for testing
+touch tmp_file
+chmod 755 tmp_file
 
 if [ -d /var/lib/mysql/mysql ]; then
 	echo "Already set up, starting..."
@@ -15,7 +30,8 @@ chown -R mysql:mysql /var/lib/mysql
 
 mariadb-install-db --auth-root-authentication-method=normal --basedir=/usr --datadir=/var/lib/mysql --skip-test-db --user=mysql
 
-cat << break > tmp_mdb
+# Write setup database and users in temporary file
+cat << break > tmp_file
 FLUSH PRIVILEGES;
 DELETE FROM mysql.user WHERE User = 'root' AND Host != 'localhost';
 SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$DB_ROOT_PASS');
@@ -24,8 +40,9 @@ GRANT ALL ON $DB_NAME.* TO '$DB_USER_NAME'@'%' IDENTIFIED BY '$DB_USER_PASS';
 FLUSH PRIVILEGES;
 break
 
-/usr/bin/mariadbd --user=mysql --bootstrap --skip-name-resolve --skip-networking=0 < tmp_mdb
+/usr/bin/mariadbd --user=mysql --bootstrap --skip-name-resolve --skip-networking=0 < tmp_file
 
-rm -f tmp_mdb
+# Remove temporary file
+rm -f tmp_file
 
 exec /usr/bin/mariadbd --user=mysql --skip-networking=0
