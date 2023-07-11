df8a2921fb253418e8099bf3f02b7d0a6b2b264f
jfremond <jfremond@student.42.fr>
2023-07-11 19:07:09 +0200

    :warning: TEST

--- srcs/requirements/mariadb/tools/db_setup.sh
+++ srcs/requirements/mariadb/tools/db_setup.sh
@@ -1,20 +1,18 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    entrypoint.sh                                      :+:      :+:    :+:    #
+#    db_setup.sh                                        :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: jfremond <jfremond@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/06/30 20:51:33 by jfremond          #+#    #+#              #
-#    Updated: 2023/06/30 20:57:56 by jfremond         ###   ########.fr        #
+#    Updated: 2023/07/11 15:58:39 by jfremond         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-sleep 5
-
-# Set up database if not already setup
-# --user=mysql ->
-# --skip-networking=0 ->
+# Set up database if not already set
+# --user=mysql -> Run mariadbd daemon as mysql (default user)
+# --skip-networking=0 -> listen for TCP/IP conections
 if [ -d /var/lib/mysql/mysql ]; then
 	echo "Already set up, starting..."
 	exec /usr/bin/mariadbd --user=mysql --skip-networking=0
@@ -26,36 +24,38 @@ echo "Installing..."
 mkdir -p /var/lib/mysql
 chown -R mysql:mysql /var/lib/mysql
 
-# --auth-root-authentication-method=normal -> 
-# --basedir=/usr ->
-# --datadir=/var/lib/mysql ->
-# --skip-test-db ->
-#--user=mysql ->
-mariadb-install-db --auth-root-authentication-method=normal --basedir=/usr --datadir=/var/lib/mysql --skip-test-db --user=mysql
+# --auth-root-authentication-method=normal -> Create a root@localhost that authenticates with a password
+# --basedir=/usr -> Path to the MariaDB installation directory
+# --datadir=/var/lib/mysql -> Directory where the data is stored
+# --skip-test-db -> Don't install the test database
+# --user=mysql -> Run mariadbd daemon as mysql (default user)
+/usr/bin/mariadb-install-db --auth-root-authentication-method=normal --basedir=/usr --datadir=/var/lib/mysql --skip-test-db --user=mysql
 
 # Write setup database and users in temporary file
-#
-#
-#
-#
-#
+# Initialize privileges table
+# Delete root if not localhost
+# Give a password to root
+# Create a second user and give it a password
+# Give all rights to the second user
+# Apply modifications to the privileges table
 cat << break > tmp_file
 FLUSH PRIVILEGES;
 DELETE FROM mysql.user WHERE User = 'root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$DB_ROOT_PASS');
 CREATE USER '$DB_USER_NAME'@'%' IDENTIFIED BY '$DB_USER_PASS';
 GRANT ALL ON $DB_NAME.* TO '$DB_USER_NAME'@'%' IDENTIFIED BY '$DB_USER_PASS';
+FLUSH PRIVILEGES;
 break
 
-# --user=mysql -> 
-# --bootstrap -> 
-# --skip-name-resolve -> 
-# --skip-networking=0 ->
+# --user=mysql -> Run mariadbd daemon as mysql (default user)
+# --bootstrap -> Initialize the server and set up the necessary system tables and data
+# --skip-name-resolve -> Only use IP addresses for connections
+# --skip-networking=0 -> listen for TCP/IP conections
 /usr/bin/mariadbd --user=mysql --bootstrap --skip-name-resolve --skip-networking=0 < tmp_file
 
 # Remove temporary file
 rm -f tmp_file
 
-# --user=mysql ->
-# --skip-networking=0 ->
+# --user=mysql -> Run mariadbd daemon as mysql (default user)
+# --skip-networking=0 -> listen for TCP/IP conections
 exec /usr/bin/mariadbd --user=mysql --skip-networking=0
---
+++ srcs/requirements/wordpress/tools/wp_setup.sh
@@ -0,0 +1,48 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    wp_setup.sh                                        :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: jfremond <jfremond@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2023/07/01 02:44:25 by jfremond          #+#    #+#              #
+#    Updated: 2023/07/11 13:44:28 by jfremond         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+#!/bin/bash
+
+sleep 10
+
+set -x
+
+echo "Installing..."
+cd /var/www/wordpress
+# Generate a wp-config file
+# Create a new database
+# Run the standard WordPress installation process
+# Create a new user
+if [ ! -f /var/www/wordpress/wp-config.php ]; then
+	wp config create	--allow-root \
+						--dbname=$DB_NAME \
+						--dbuser=$DB_USER_NAME \
+						--dbpass=$DB_USER_PASS \
+						--dbhost=mariadb:3306 --path='/var/www/wordpress'
+	wp db create
+
+	wp core install	--allow-root \
+					--url=$DOMAIN_NAME \
+					--title=$SITE_TITLE \
+					--admin_user=$WP_ADMIN_NAME \
+					--admin_password=$WP_ADMIN_PASS \
+					--admin_email=$WP_ADMIN_MAIL \
+					--path='/var/www/wordpress'
+
+	wp user create	--allow-root \
+					--role=author $WP_USER_NAME $WP_USER_MAIL \
+					--user_pass=$WP_USER_PASS \
+					--path='/var/www/wordpress'
+fi
+
+# Run PHP-FPM in foreground
+exec /usr/sbin/php-fpm81 -F
